078d8ff7d2aaf9058c9c55f9a2ec25e93a3db8bd
fagami1423 <magar.fagami@gmail.com>
2018-03-09 19:20:27 +0545

    form for adding PAT

--- template/admin/addPATForm.php
+++ template/admin/addPATForm.php
@@ -1,12 +1,43 @@
 <form method="POST" action="add-PAT.php">
     <h2 class="text-center">Add Personal Tutor</h2>
-    <?php 
-        $formData = [["text", "tutorfName", "Tutor First Name"], ["text", "tutorlName", "Tutor Surname"]];
-        $getRow = [];
-        require '../../functions/form-generate-function.php';
-    ?>
-    <div>
-    <div class="form-group text-center">
-        <input type="submit" name="submit" value="Add Course" class="btn btn-primary">
+    <div class="form-group">
+        <label for="studentOption">Select Student</label>
+        <select class="form-control" id="studentOption" name="Stid">
+            <?php 
+                require '../../databaseConnect/connectSQL.php';
+                require'../../classes/databaseQuery.php';
+                $studentObj = new QueryDatabase($pdo, 'student');
+                $stmt=$studentObj->findAll();
+                echo '<ul>';
+                foreach($stmt as $row){?>
+                    <option value="<?php echo $row['Stid']; ?>">
+                       <?php echo $row['studentFirstName'].' '.$row['studentSurName'];?>
+                    </option>
+             <?php   }
+                echo '</ul>';
+            ?>
+        </select>
     </div>
+    <div class="form-group">
+        <label for="staffOption">Select Staff</label>
+        <select class="form-control" id="staffOption" name="staffId">
+            <?php 
+               // require '../../databaseConnect/connectSQL.php';
+                $staffObj = new QueryDatabase($pdo, 'staff');
+                $stmt=$staffObj->findAll();
+                echo '<ul>';
+                foreach($stmt as $row){?>
+                    <option value="<?php echo $row['staffId']; ?>">
+                       <?php echo $row['staffFirstName'].' '.$row['staffSurName'];?>
+                    </option>
+             <?php   }
+                echo '</ul>';
+            ?>
+        </select>
+    </div>
+     
+    <div class="form-group text-center">
+        <input type="submit" name="submit" value="Add Module" class="btn btn-primary">
+    </div>
+   
 </form>
